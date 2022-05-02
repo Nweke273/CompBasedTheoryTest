1c208ae2372a66d3c70ec71a0cafbf095b163e01
Nweke273 <chisom5710@gmail.com>
2022-05-02 17:33:25 +0100

    make faker dependency

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,13 +15,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => "admin@gmail.com",//$this->faker->unique()->safeEmail(),
-            'role' => $this->faker->randomElement($array = array(
-                '2',
-            )),
+            'name' => 'Admin',
+            'email' => "admin@gmail.com",
+            'role' => 2,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'remember_token' => Str::random(10),
         ];
     }
